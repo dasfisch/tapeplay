253617278398f5579832991211f83c294dd91511
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-04-09 15:41:58 -0500

    lots of home page

--- home.php
+++ home.php
@@ -1,3 +1,13 @@
 <?php
-    include_once('templates/header.php');
+    include_once('templates/common/header.php');
 ?>
+
+<?php
+    include_once('templates/home.php');
+?>
+
+<?php
+    include_once('templates/common/footer.php');
+?>
+</body>
+</html>
