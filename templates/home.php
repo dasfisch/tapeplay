a5738ceabbf029c7ae1b4df4e47bd86de60f280b
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-04-09 15:41:45 -0500

    lots of home page

---
+++ templates/home.php
@@ -0,0 +1,121 @@
+<div id="main">
+    <div id="centerAd">Hello Ad</div>
+    <h1>Video makes the world go round.</h1>
+    <h3>The world's evolved. So has recruiting.</h3>
+    <div id="searchForm">
+        <form id="search" action="/search" method="post">
+            <div class="inputField">
+                <div class="left"></div>
+                <div class="middle">
+                    <input type="text" class="standard" id="searchVal" name="searchVal" />
+                </div>
+                <div class="right"></div>
+            </div>
+            <div class="blackButton">
+                <div class="topLeft"></div>
+                <div class="topRight"></div>
+                <div class="bottomLeft"></div>
+                <div class="bottomRight"></div>
+                <div class="middle">
+                    <input type="submit" value="Search" id="sendSearch" class="largeBlack" />
+                </div>
+            </div>
+
+            <input type="hidden" name="level" id="level" value="" />
+            <input type="hidden" name="grade" id="grade" value="" />
+            <input type="hidden" name="position" id="position" value="" />
+            <input type="hidden" name="height" id="height" value="" />
+
+            <div id="advance">
+                <p class="italic">Add more search criteria</p>
+                <div id="options">
+                    <div class="option">
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="levelName">High School</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="levelName">College</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="levelName">Professional</div>
+                        </div>
+                    </div>
+                    <div class="option">
+                        <div id="sportSelect">
+                            <div id="pickSport">Pick Sport:</div>
+                            <div id="dropper">
+                                <div id="values">
+                                    <p id="value">Women's Basketball</p>
+                                    <ul id="potentials">
+                                        <li>Women's Baseketball</li>
+                                        <li>Men's Baseketball</li>
+                                    </ul>
+                                </div>
+                                <div id="arrow"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="option">
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="position">Point Guard</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="position">Shooting Guard</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="position">Small Forward</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="position">Power Forward</div>
+                        </div>
+                        <div class="checkbox">
+                            <div class="box">
+                                <div class="arrow"></div>
+                            </div>
+                            <div class="position">Center</div>
+                        </div>
+                    </div>
+                    <div class="option">
+                        <div id="sportSelect">
+                            <div id="pickSport">Pick Sport:</div>
+                            <div id="dropper">
+                                <div id="values">
+                                    <p id="value">Women's Basketball</p>
+                                    <ul id="potentials">
+                                        <li>Women's Baseketball</li>
+                                        <li>Men's Baseketball</li>
+                                    </ul>
+                                </div>
+                                <div id="arrow"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<div id="ad">
+    <h1>Ad</h1>
+</div>
