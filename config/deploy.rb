a71f436e58607305d791bd7ffaaff29b8312a58c
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-05-07 21:27:28 -0500

    Adding capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'capistrano/ext/multistage'
+load 'deploy'
+
+#set :application, "uxwordpress"
+#set(:repository) { "git@git301p.prod.ch3.s.com:uxwpress.git" }
+
+##### SOURCE CONTROL #####
+set :scm, :git
+
+##### DEPLOYMENT #####
+#set(:user) { "stateadm" }
+#set(:deploy_to) { "/opt/stateapps/uxwpress" }
+set :deploy_via, :remote_cache
+#set :branch, "#{:appBranch}"
+set :use_sudo, false
+
+namespace :deploy do
+
+  desc "A macro-task that updates the code and fixes the symlink."
+  task :default do
+    transaction do
+      update_code
+      symlink
+    end
+  end
+
+  task :update_code, :except => { :no_release => true } do
+    on_rollback { run "rm -rf #{release_path}; true" }
+    strategy.deploy!
+  end
+
+  task :after_deploy do
+    cleanup
+  end
+
+  task :after_symlink do
+  end
+end
---
+++ config/deploy/tp_prod_1.rb
@@ -0,0 +1,16 @@
+#testing something on qa
+set(:user) { "tapeplayer" }
+
+set(:domain) { "ec2-50-19-56-168.compute-1.amazonaws.com" }
+set(:application) { "tapeplay" }
+set(:repository) { "git@barable.com:30000/home/git/repos/tapeplay.git" }
+
+set(:keep_releases) {1}
+
+##### APPLICATION #####
+role :web, "#{domain}"  # Your HTTP server, Apache/etc
+role :app, "#{domain}"  # This may be the same as your `Web` server
+role :db, domain, :primary => true
+
+set (:branch) { "qa" }
+set (:deploy_to) { "/apps/tapeplay/www" }
