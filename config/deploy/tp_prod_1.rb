107c83129bf8b4d2c79ef7c0d285335faf4d3442
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-05-08 10:13:57 -0500

    Fixed deployment issue

--- config/deploy/tp_prod_1.rb
+++ config/deploy/tp_prod_1.rb
@@ -1,9 +1,11 @@
 #testing something on qa
 set(:user) { "tapeplayer" }
 
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "tapeplayer")]
+
 set(:domain) { "ec2-50-19-56-168.compute-1.amazonaws.com" }
 set(:application) { "tapeplay" }
-set(:repository) { "git@barable.com:30000/home/git/repos/tapeplay.git" }
+set(:repository) { "ssh://git@barable.com:30000/home/git/repos/tapeplay.git" }
 
 set(:keep_releases) {1}
 
@@ -14,4 +16,4 @@ role :db, domain, :primary => true
 
 set (:branch) { "development" }
 set (:deploy_to) { "/apps/tapeplay/www" }
-set (:port) { 30000 }
+set :deploy_via, :copy
