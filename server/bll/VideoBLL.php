a23dc0ab55b4e80475cfddb6318dd88773011ddf
tcontosta <tcontosta@gmail.com>
2012-05-20 21:45:31 -0400

    Added JWPlayer files Updated Panda stuff. HTML now being output for HTML5/Flash player.

--- server/bll/VideoBLL.php
+++ server/bll/VideoBLL.php
@@ -3,14 +3,19 @@
 namespace tapeplay\server\bll;
 
 require_once("bll/BaseBLL.php");
+require_once("bll/Panda.php");
 require_once("dal/VideoDAO.php");
 require_once("model/Video.php");
+require_once("utility/PandaUtil.php");
+require_once("enum/PandaProfileTypes.php");
 
 use tapeplay\server\dal\VideoDAO;
 use tapeplay\server\model\Video;
+use tapeplay\server\bll\Panda;
 
 class VideoBLL extends BaseBLL
 {
+
 	function __construct()
 	{
 		$this->dal = new VideoDAO();
@@ -20,8 +25,70 @@ class VideoBLL extends BaseBLL
 	 * @param \tapeplay\server\model\Video $video The video that needs to be inserted
 	 * @return int The id of the video that was inserted.  -1 if insert fails
 	 */
-	function insert(Video $video)
+	public function insert(Video $video)
 	{
 		return $this->dal->insert($video);
+	}
+
+	/**
+	 * Returns all HTML/JS necessary to display the video.
+	 * @param $videoID string The Panda Video ID that we need to get the Amazon S3 encodings
+	 * @return string The HTML that contains the HTML5 tag and JWPlayer JavaScript.
+	 */
+	public function getFullVideoHTML($videoID)
+	{
+		$panda = new Panda();
+
+		// grab the encodings from the REST resources
+		$encodings = json_decode(@$panda->get("/videos/" . $videoID . "/encodings.json"));
+
+		// get video width and height based on first encoding
+		$width = $encodings[0]->width;
+		$height = $encodings[0]->height;
+
+		// grab mp4 webm, and ogg
+		$sourceHTML = "";
+		$sourceHTML .= $this->getEncoding($encodings, \PandaProfileTypes::$H264);
+		$sourceHTML .= $this->getEncoding($encodings, \PandaProfileTypes::$WEBM);
+		$sourceHTML .= $this->getEncoding($encodings, \PandaProfileTypes::$OGG);
+
+		// insert video attributes and source into video tag
+		$html = \PandaUtil::$VIDEO_TAG;
+		$html = str_replace("{encodings}", $sourceHTML, $html);
+		$html = str_replace("{width}", $width, $html);
+		$html = str_replace("{height}", $height, $html);
+
+		// grab the jquery call to the jwplayer
+		$js = \PandaUtil::$JW_PLAYER;
+
+		return $html . $js;
+	}
+
+	/**
+	 * @param array $encodings The list of encodings retrieved for the video.
+	 * @param $type string The name of the encoding that we want to retrieve.
+	 * @return string The encoding that matches the requested one.
+	 */
+	private function getEncoding(array $encodings, $type)
+	{
+		// get default template for the source tag
+		$html = \PandaUtil::$SOURCE_TAG;
+
+		// loop through encodings and get the correct match
+		foreach($encodings as $encoding)
+		{
+			if ($encoding->profile_name == $type)
+			{
+				// place the video location here
+				$html = str_replace("{source}", \PandaUtil::$S3_BUCKET_URL . $encoding->path . $encoding->extname, $html);
+
+				// remove the "." from the extension and add extension
+				$html = str_replace("{type}", $encoding->mime_type, $html);
+
+				break;
+			}
+		}
+
+		return $html;
 	}
 }
---
+++ server/enum/PandaVerbTypes.php
@@ -0,0 +1,9 @@
+<?php
+
+class PandaVerbTypes
+{
+	public static $GET = "GET";
+	public static $POST = "POST";
+	public static $PUT = "PUT";
+	public static $DELETE = "DELETE";
+}
