b65f07a9fbb72e88d496fd332b02d37fe0aeb03c
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-05-20 19:18:51 -0500

    Fix for ticket:8; adding changes from comment:3

--- templates/about.php
+++ templates/about.php
@@ -2,7 +2,7 @@
     <div id="leftCol" class="helpCenter">
         <h2>About TapePlay</h2>
         <div class="help">
-            <h3>We Exist to Make Recruiting Better</h3>
+            <h3>We exist to make recruiting better.</h3>
             <p>
                 For players, it means helping talented athletes get the important exposure they
                 need to get to the next level. You upload a video and we put it in front of coaches
@@ -12,7 +12,7 @@
                 For scouts and coaches, it means putting an end to the days of sifting through
                 dozens of DVDs with a clipboard and pencil. We've brought recruiting into the digital
                 world. Now, you can search through player videos by specific criteria, save videos,
-                and use <strong>VideoNotes</strong> to instantly share notes and time stamps with your entire staff.
+                and use <span class="bold">VideoNotes</span> to instantly share notes and time stamps with your entire staff.
             </p>
             <p>
                 The world's evolved. So has recruiting. Welcome aboard.
