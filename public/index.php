8b2a08b75e01dfeced0c7d91d8296e31a9f93f04
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-12-03 22:06:14 -0600

    Base server tests

--- public/index.php
+++ public/index.php
@@ -3,10 +3,19 @@
 
 	    //Register an autoloader
 	    $loader = new \Phalcon\Loader();
+
 	    $loader->registerDirs(array(
 	        '../server/controllers/',
 	        '../server/models/'
-	    ))->register();
+	    ));
+
+//		$loader->registerClasses(
+//		    array(
+//		        "Player_Stats"         => "../server/models/Player_Stats.php",
+//		    )
+//		);
+
+		$loader->register();
 
 	    //Create a DI
 	    $di = new Phalcon\DI\FactoryDefault();
@@ -24,17 +33,31 @@
 	            "host" => "127.0.0.1:3306",
 	            "username" => "root",
 	            "password" => "n3uraxis",
-	            "dbname" => "dev_tapeplay"
+	            "dbname" => "tapeplay_phase_2"
 	        ));
 	    });
 
-	    //Handle the request
-	    $application = new \Phalcon\Mvc\Application();
-	    $application->setDI($di);
+		//Set the models cache service
+		$di->set('modelsCache', function(){
+		    // Cache data for one day by default
+		    $frontCache = new Phalcon\Cache\Frontend\Data(array(
+		        "lifetime" => 86400
+		    ));
 
-		echo '<pre>';
-		var_dump($application->getDI()->get('db'));
-		exit;
+		    // Memcached connection settings
+		    $cache = new Phalcon\Cache\Backend\Memcache($frontCache, array(
+		        "host"      => "localhost",
+		        "port"      => "11211",
+			    "statsKey"  => 'tp_cache_',
+			    'persistent' => false
+		    ));
+
+		    return $cache;
+		});
+
+	    //Handle the request
+		$application = new \Phalcon\Mvc\Application();
+        $application->setDI($di);
 
 	    echo $application->handle()->getContent();
 	} catch(PDOException $e) {
