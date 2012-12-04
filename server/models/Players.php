417fa31e9993108ba616df7f943a4f901969638c
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-12-04 00:22:40 -0600

    Fixed up all of the relational stuff; now I have a good grip on it

--- server/models/Players.php
+++ server/models/Players.php
@@ -1,6 +1,9 @@
 <?php
 	class Players extends \Phalcon\Mvc\Model {
 		public function initialize() {
+			/**
+			 * I know how this works; I just don't know how to explain it to you :|
+			 */
 			$this->belongsTo('user_id', 'Users', 'id');
 
 			$this->hasMany('id', 'Videos', 'player_id');
